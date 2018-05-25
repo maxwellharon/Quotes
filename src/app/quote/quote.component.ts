689d71695396cbda06516bc5b3e293e733f73277
maxwellharon <maxwellharon@gmail.com>
2018-05-25 15:14:02 +0300

    Add delete button

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote'
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+  quotes = [
+      new Quote(1,'It is only a fool who tests the depth of water with both feet','Hey Anilla'),
+      new Quote(2,'Buy Cookies','Hey Anilla'),
+      new Quote(3, 'Get new Phone Case','Hey Anilla'),
+      new Quote(4, 'Get Dog Food','Hey Anilla'),
+      new Quote(5, 'Solve math homework','Hey Anilla'),
+      new Quote(6, 'Plot my world domination plan','Hey Anilla'),
+
+  ]
+  toogleDetails(index){
+       this.quotes[index].showDescription = !this.quotes[index].showDescription;
+   }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
