fbe8c0dca69eb312b5265837b64521552058b1e0
maxwellharon <maxwellharon@gmail.com>
2018-05-25 17:17:13 +0300

    Add form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,13 @@ import {Quote} from './quote'
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-    quotes = [
-        new Quote(1,'It is only a fool who tests the depth of water with both feet'),
-        new Quote(2,'Buy Cookies',new Date(20/8/18)),
-        new Quote(3, 'Get new Phone Case',new Date(20/8/18)),
-        new Quote(4, 'Get Dog Food',new Date(20/8/18)),
-        new Quote(5, 'Solve math homework',new Date(20/8/18)),
-        new Quote(6, 'Plot my world domination plan',new Date(20/8/18)),
+  quotes = [
+      new Quote(1,'It is only a fool who tests the depth of water with both feet','Hey Anilla',new Date(20,8,18)),
+      new Quote(2,'Buy Cookies','Hey Anilla',new Date(20,8,18)),
+      new Quote(3, 'Get new Phone Case','Hey Anilla',new Date(20,8,18)),
+      new Quote(4, 'Get Dog Food','Hey Anilla',new Date(20,8,18)),
+      new Quote(5, 'Solve math homework','Hey Anilla',new Date(20,8,18)),
+      new Quote(6, 'Plot my world domination plan','Hey Anilla',new Date(20,8,18)),
 
-    ]
+  ]
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,12 @@ import {Quote} from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes = [
-      new Quote(1,'It is only a fool who tests the depth of water with both feet','Hey Anilla'),
-      new Quote(2,'Buy Cookies','Hey Anilla',new Date(20/8/18)),
-      new Quote(3, 'Get new Phone Case','Hey Anilla',new Date(20/8/18)),
-      new Quote(4, 'Get Dog Food','Hey Anilla',new Date(20/8/18)),
-      new Quote(5, 'Solve math homework','Hey Anilla',new Date(20/8/18)),
-      new Quote(6, 'Plot my world domination plan','Hey Anilla',new Date(20/8/18)),
+      new Quote(1,'It is only a fool who tests the depth of water with both feet','Hey Anilla',new Date(20,8,18)),
+      new Quote(2,'Buy Cookies','Hey Anilla',new Date(20,8,18)),
+      new Quote(3, 'Get new Phone Case','Hey Anilla',new Date(20,8,18)),
+      new Quote(4, 'Get Dog Food','Hey Anilla',new Date(20,8,18)),
+      new Quote(5, 'Solve math homework','Hey Anilla',new Date(20,8,18)),
+      new Quote(6, 'Plot my world domination plan','Hey Anilla',new Date(20,8,18)),
 
   ]
   completeQuote(isComplete,index){
@@ -26,8 +26,8 @@ export class QuoteComponent implements OnInit {
        this.quotes[index].showDescription = !this.quotes[index].showDescription;
    }
 
-//   constructor() { }
-//
-//   ngOnInit() {
-//   }
-// }
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
