20b629cfae080538b6bd6ab2d36f275acec539db
maxwellharon <maxwellharon@gmail.com>
2018-05-28 11:15:38 +0300

    add D

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,7 +11,7 @@ export class QuoteComponent implements OnInit {
       new Quote(1,'It is only a fool who tests the depth of water with both feet','African Proverb',new Date(20,8,18)),
       new Quote(2,'The two most important days in your life are the day you are born and the day you find out why','Mark Twain',new Date(20,8,18)),
       new Quote(3, 'We are all in the gutter, but some of us are looking at the stars','Oscar Wilde',new Date(20,8,18)),
-      new Quote(4, 'ream as if you will live forever. Live as if you will die today.','James Dean',new Date(20,8,18)),
+      new Quote(4, 'Dream as if you will live forever. Live as if you will die today.','James Dean',new Date(20,8,18)),
       new Quote(5, 'If a man can bridge the gap between life and death, if he can live on after he is dead, then maybe he was a great man.','Hey Anilla',new Date(20,8,18)),
       new Quote(6, 'Government of the people, by the people, for the people, shall not perish from the Earth.','Abraham Lincon',new Date(20,8,18)),
 
